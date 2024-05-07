f9bfc8e78120f08e9f0aff07fc99c9dc2b2eded2
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-05-07 18:20:09 +0700

    feat fetch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,33 @@
-// import About from "./components/fragments/About";
-// import ActionSection from "./components/fragments/ActionSection";
-// import BankPartners from "./components/fragments/BankPartners";
-// import Footer from "./components/fragments/Footer";
-// import Header from "./components/fragments/Header";
-// import Navbar from "./components/fragments/Navbar";
-// import NavbarTop from "./components/fragments/NavbarTop";
-// import TestimonialSection from "./components/fragments/TestimonialSection";
-// import TrainingSection from "./components/fragments/TrainingSection";
-// import VideosSection from "./components/fragments/VideosSection";
-// import WhyMeSection from "./components/fragments/Whyme";
+import About from "./components/fragments/About";
+import ActionSection from "./components/fragments/ActionSection";
+import BankPartners from "./components/fragments/BankPartners";
+import Footer from "./components/fragments/Footer";
+import Header from "./components/fragments/Header";
+import Navbar from "./components/fragments/Navbar";
+import NavbarTop from "./components/fragments/NavbarTop";
+import TestimonialSection from "./components/fragments/TestimonialSection";
+import TrainingSection from "./components/fragments/TrainingSection";
+import VideosSection from "./components/fragments/VideosSection";
+import WhyMeSection from "./components/fragments/Whyme";
 
-// async function getData() {
-//   const res = await fetch("http://localhost:3000/api/data-web", {
-//     cache: "no-store",
-//   });
+async function getData() {
+  const res = await fetch("https://app-remax-web.vercel.app/api/data-web", {
+    cache: "no-store",
+  });
 
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
 export default async function Home() {
-  // const data = await getData();
+  const data = await getData();
 
   return (
     <main className="min-h-screen">
-      test
-      {/* <div className="top-0 z-[99] sticky ">
+      <div className="top-0 z-[99] sticky ">
         <NavbarTop {...data.data} />
         <Navbar {...data.data} />
       </div>
@@ -42,7 +41,7 @@ export default async function Home() {
       <TestimonialSection {...data.data} />
       <div className="w-full bg-[#014389]">
         <Footer />
-      </div> */}
+      </div>
     </main>
   );
 }
