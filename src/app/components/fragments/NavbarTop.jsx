8fb3371e3bd7020c345f4c59c232440a497a47b4
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-05-13 17:21:32 +0700

    feat navbar

--- src/app/components/fragments/NavbarTop.jsx
+++ src/app/components/fragments/NavbarTop.jsx
@@ -9,9 +9,9 @@ const NavbarTop = ({ topNavigations }) => {
       initial={{ opacity: 0, y: -300 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, ease: "easeInOut" }}
-      className="bg-blue-remax h-10 max-sm:h-16 flex items-center  "
+      className="bg-blue-remax h-10 max-sm:h-16 flex items-center  justify-center"
     >
-      <div className="container mx-auto  px-8 flex sm:divide-x-2 justify-center sm:max-w-[1250px]">
+      <div className="container mx-auto w-fit max-sm:scale-95 px-8 flex sm:divide-x-2 max-sm:justify-center sm:max-w-[1250px]">
         {topNavigations.map((item, index) => (
           <IconTextName
             key={index}
